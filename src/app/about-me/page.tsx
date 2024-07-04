eb8d65b7aa06e0734e062b5a8b06998823d700ee
Khanh Hoa <hoa.lo@phoenixunity.io>
2024-07-04 15:32:12 +0700

    update gitignore

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -1,11 +1,6 @@
-import { getAccountSever } from "@/service/accout";
-import { cookies } from "next/headers";
 import Profile from "./Profile";
 
 async function AboutMe() {
-  const cookieStore = cookies();
-  const token = cookieStore.get('token')
-  const data = await getAccountSever(token?.value);
   return (
     <div>
       <div>AboutMe</div>
